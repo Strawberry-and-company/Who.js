bbeee6549b779bf1b4d19aa44732b2c07ed892fa
Strawberry-and-company <40215163+Strawberry-and-company@users.noreply.github.com>
2021-03-29 10:42:02 +0200

    Create who.js

---
+++ src/who.js
@@ -0,0 +1,21 @@
+class who {
+    constructor(element, event) {
+        this.element = element;
+        this.event = null;
+        this.callback = null;
+    }
+    over() {
+
+    }
+    addclass() {
+        this.element.a
+    }
+
+    removeclass() {
+
+    }
+}
+
+// example
+var p = new who("p");
+console.log(p);
